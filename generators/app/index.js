30dbae170c3423eae4391f4c1120d92deb1f5979
Brandon <brandonbsuen@gmail.com>
2017-10-01 09:12:40 -0700

    Prompt set up for preferred tool for dependency management

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,9 +10,28 @@ module.exports = class extends Generator {
     super(args, opts);
 
     this.props = {
+      manager: 'Yarn',
       template: 'Pug, formerly known as Jade',
       preprocessor: 'Sass',
       js: 'ECMAScript 6 (ES6)/ ECMAScript 2015 (ES2015)'
+    };
+
+    this.promptingManagers = function() {
+
+      return this.prompt({
+        type: 'list',
+        name: 'manager',
+        message: 'Which option would you like to use for dependency management?',
+        choices: [
+          'NPM',
+          'Yarn'
+        ],
+        default: 'Yarn'
+      }).then(props => {
+        this.props.manager = props.manager;
+        this.prompting();
+      });
+
     };
 
     this.promptingTemplates = function(){
@@ -30,10 +49,8 @@ module.exports = class extends Generator {
         ],
         default: 'Pug, formerly known as Jade'
       }).then(props => {
-
         this.props.template = props.template;
         this.prompting();
-
       });
 
     };
@@ -53,10 +70,8 @@ module.exports = class extends Generator {
         ],
         default: 'Sass'
       }).then(props => {
-
         this.props.preprocessor = props.preprocessor;
         this.prompting();
-
       });
 
     };
@@ -78,87 +93,29 @@ module.exports = class extends Generator {
         ],
         default: 'ECMAScript 6 (ES6)/ ECMAScript 2015 (ES2015)'
       }).then(props => {
-
         this.props.js = props.js;
         this.prompting();
-
       });
 
-    }
-
-    /*this.customize = function() {
-      return this.prompt({
-        type: 'list',
-        name: 'customize',
-        message: 'Would you like to stick with the defaults, or make this project yours?',
-        choices: [
-          'Customize',
-          'Defaults'
-        ],
-        default: 'Defaults'
-      }).then(props => {
-        this.props.customize = props.customize;
-      });
-    };*/
-
-
-
-    //this.promptLoop = promptLoop;
-
-    /*this.template = function() {
-      return this.prompt({
-        type: 'list',
-        name: 'template',
-        message: 'Which template engine, if any, would you like to use for HTML?',
-        choices: [
-          'EJS',
-          'Handlebars',
-          'Mustache',
-          'Plain old HTML',
-          'Pug, formerly known as Jade'
-        ],
-        default: 'Pug, formerly known as Jade'
-      }).then(props => {
-        this.props.template = props.template;
-      });
     };
 
-    this.preprocessor = function() {
-      return this.prompt({
-        type: 'list',
-        name: 'preprocessor',
-        message: 'Which CSS pre-processor, if any, would you like to use?',
-        choices: [
-          'Less',
-          'Plain old CSS',
-          'Sass',
-          'SCSS, Sass with more CSS-like syntax',
-          'Stylus'
-        ],
-        default: 'Sass'
-      }).then(props => {
-        this.props.preprocessor = props.preprocessor;
-      });
-    };
+    /*this.promptingJSLib = function() {
 
-    this.js = function() {
       return this.prompt({
-        type: 'list',
-        name: 'js',
-        message: 'Which version or alternative of Javascript do you prefer?',
+        type: 'checkbox',
+        name: 'jslib',
+        message: 'Which Javascript libraries, if any, would you like to use?',
         choices: [
-          'CoffeeScript',
-          'ECMAScript 5 (ES5), what you probably know as vanilla JS',
-          'ECMAScript 6 (ES6)/ ECMAScript 2015 (ES2015)',
-          'ECMAScript 2016 (ES2016)',
-          'ECMAScript 2017 (ES2017)',
-          'Opal, compiles Ruby to JS',
-          'TypeScript'
+          'D3',
+          'jQuery',
+          'Lodash',
+          'Underscore'
         ],
-        default: 'ECMAScript 6 (ES6)/ ECMAScript 2015 (ES2015)'
+        default:
       }).then(props => {
-        this.props.js = props.js;
+        this.props.jslib
       });
+
     };*/
 
   }
@@ -205,13 +162,17 @@ module.exports = class extends Generator {
       message: 'Are you alright with your current settings? Select what applies to you.',
       choices: [
         'I\'m ready to generate!',
+        `I wouldn\'t like to use ${chalk.red(this.props.manager)} for dependency management`,
         `I wouldn\'t like to use ${short[this.props.template]} template engine`,
         `I wouldn\'t like to use ${short[this.props.preprocessor]} pre-processor`,
         `I wouldn\'t like to use ${short[this.props.js]}`
       ],
       default: 'I\'m ready to generate!'
     }).then(props => {
-      if (props.settings.endsWith('engine')) {
+      if (props.settings.endsWith('management')) {
+        this.promptingManagers();
+      }
+      else if (props.settings.endsWith('engine')) {
         this.promptingTemplates();
       }
       else if (props.settings.endsWith('pre-processor')) {
@@ -224,99 +185,6 @@ module.exports = class extends Generator {
         done();
       }
     });
-
-    /*this.log("HI");
-    //this.customize();
-    if (this.props.customize == 'Customize') {
-
-      while (this.props.settings != 'I\'m ready to generate!') {
-
-        //this.settings();
-        if (this.props.settings.endsWith('engine')) {
-          //this.template();
-        }
-        else if (this.props.settings.endsWith('pre-processor')) {
-          //this.preprocessor();
-        }
-        else if (this.props.settings.endsWith('Javascript')) {
-          //this.js();
-        }
-
-      }
-
-    }
-    return this.settings({
-      type: 'list',
-      name: 'settings',
-      message: 'Are you alright with your current settings? If not, select what you\'d like to change.',
-      choices: [
-        'I\'m ready to generate!',
-        `I wouldn\'t like to use ${short[this.props.template]} template engine`,
-        `I wouldn\'t like to use ${short[this.props.preprocessor]} pre-processor`,
-        `I wouldn\'t like to use ${short[this.props.js]}`
-      ],
-      default: 'I\'m ready to generate!'
-    }).then(function(props) {
-
-    });
-
-    this.template = function() {
-      return this.prompt({
-        type: 'list',
-        name: 'template',
-        message: 'Which template engine, if any, would you like to use for HTML?',
-        choices: [
-          'EJS',
-          'Handlebars',
-          'Mustache',
-          'Plain old HTML',
-          'Pug, formerly known as Jade'
-        ],
-        default: 'Pug, formerly known as Jade'
-      }).then(props => {
-        this.props.template = props.template;
-      });
-    };
-
-    this.preprocessor = function() {
-      return this.prompt({
-        type: 'list',
-        name: 'preprocessor',
-        message: 'Which CSS pre-processor, if any, would you like to use?',
-        choices: [
-          'Less',
-          'Plain old CSS',
-          'Sass',
-          'SCSS, Sass with more CSS-like syntax',
-          'Stylus'
-        ],
-        default: 'Sass'
-      }).then(props => {
-        this.props.preprocessor = props.preprocessor;
-      });
-    };
-
-    this.js = function() {
-      return this.prompt({
-        type: 'list',
-        name: 'js',
-        message: 'Which version or alternative of Javascript do you prefer?',
-        choices: [
-          'CoffeeScript',
-          'ECMAScript 5 (ES5), what you probably know as vanilla JS',
-          'ECMAScript 6 (ES6)/ ECMAScript 2015 (ES2015)',
-          'ECMAScript 2016 (ES2016)',
-          'ECMAScript 2017 (ES2017)',
-          'Opal, compiles Ruby to JS',
-          'TypeScript'
-        ],
-        default: 'ECMAScript 6 (ES6)/ ECMAScript 2015 (ES2015)'
-      }).then(props => {
-        this.props.js = props.js;
-      });
-    };*/
-
-    //return this.promptLoop(this.prompts[0]);
 
   }
 
